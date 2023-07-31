7ef24525f7e8b18e3c2e0280547fe41fabcd68d3
Nicole <niipimenta@hotmail.com>
2023-07-31 01:26:29 -0300

    feat: inserção de cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import connectDatabase from "./src/database/db.js";
 import calcRoute from "./src/routes/calc.router.js";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-
 connectDatabase();
+app.use(cors());
+app.use(express.json());
 
 app.use("/calculo", calcRoute);
 
